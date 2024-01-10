bf55402fa942ec4295bba2a95c75aa4d8159fd11
MarzanaBegum <marzanaBegum473@gmail.com>
2024-01-10 14:10:35 +0600

    change url

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://good-review.herokuapp.com/api",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://good-review.onrender.com/api",
 });
